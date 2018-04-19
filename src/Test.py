279be4326030e9ce9eb62d23fd62e99f4820e26b
Anja Wolf <wolf37@hm.edu>
2018-04-19 14:05:52 +0200

    self.update

--- src/Test.py
+++ src/Test.py
@@ -26,27 +26,32 @@ class Web_scraping:
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, "html.parser")
         
-        for self.item_name in soup.find_all('h1', {'class': 'heading_title'}):
-            print("Name:" + self.item_name.string)
-            
-        for self.rating_amount in soup.find_all('span', {'property': 'count'}):
+        for self.name in soup.find('h1', {'class': 'heading_title'}):
+            print("Name:" + self.name.string)
+        
+        for self.rating_amount in soup.find('span', {'property': 'count'}):
             print("Anzahl Bewertungen:" + self.rating_amount.string)
             
-        for self.price_level in soup.find_all('span', {'class': 'header_tags rating_and_popularity'}):
-            print("Preis Level:" + self.price_level.string)
-            
-        for self.item_address in soup.find_all('span', {'class': 'street-address'}):
-            print("Straße:" + self.item_address.string)
+        for self.popularity in soup.find_all('span', {'class': 'header_popularity popIndexValidation'}):
+            print("Popularität:" + self.popularity.text)
         
-        for self.item_locality in soup.find_all('span', {'class': 'locality'}):
-            print("PLZ:" + self.item_locality.string)
-            
-        ##for item_number in soup.find_all('span'):
-           ## print("Telefonnummer:" + item_number.string)
-            
-        for link in soup.find_all('a'):
-            href = link.get('href')
-            print(href)
+        for self.price_level in soup.find('span', {'class': 'header_tags rating_and_popularity'}):
+            print("Preis Level:" + self.price_level.string)
+        
+        for self.cuisine in soup.find_all('span', {'class': 'header_links rating_and_popularity'}):
+            print('Kueche:' + self.cuisine.text)
+        
+        for self.contact_details in soup.find('div', {'class': 'blRow'}):
+            print('Kontaktdaten:' + self.contact_details.text)
+        
+        for self.address in soup.find('span', {'class': 'street-address'}):
+            print("Straße:" + self.address.string)
+    
+        for self.locality in soup.find('span', {'class': 'locality'}):
+            print("PLZ:" + self.locality.string)
+        
+        for self.phonenumber in soup.find_all('div', {'class': 'blEntry phone'}):
+            print("Telefonnummer:" + self.phonenumber.text)
         
  
  
@@ -80,7 +85,7 @@ class Web_scraping:
         dataReviews = {'fruit':'orange', 'price':25}
         dataUsers = {'fruit':'orange', 'price':25}
         dataPictures = {'fruit':'orange', 'price':25}     
-        dataRestaurants = {'name':self.item_name.string, 'rating_amount':self.rating_amount.string, 'price_level':self.price_level.string, 'address':self.item_address.string, 'locality':self.item_locality.string}
+        dataRestaurants = {'name':self.name.string, 'rating_amount':self.rating_amount.string, 'price_level':self.price_level.string, 'address':self.item_address.string, 'locality':self.item_locality.string}
  
              
         #Insertion into Tables
