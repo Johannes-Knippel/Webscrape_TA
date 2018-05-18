47a900f6c90cbec4e7b06bab18cee2e5ba6c38eb
Johannes-Knippel <32730055+Johannes-Knippel@users.noreply.github.com>
2018-05-18 14:31:03 +0200

    TinyDB is working now, commented on functions and variables, some encoding is still missing

--- src/Webscraper_TripAdvisor.py
+++ src/Webscraper_TripAdvisor.py
@@ -18,18 +18,15 @@ from tinydb import TinyDB,Query,where
 
 
 
-
-
 class Web_scraping:
     
     '''
-    Created on 17.04.2018
+    Created on 18.04.2018 - 20.05.2018
     
     @author: anjawolf
     
-    get_single_data: Data will be scraped from the Tripadvisors Website and stored in variables.
+    Data will be scraped from the Tripadvisors Website and stored in variables.
     '''
-    #scrape the restaurant data from the given URL and store it in a db
     def get_single_data(self,url):
         source_code = requests.get(url)
         plain_text = source_code.text
@@ -62,23 +59,28 @@ class Web_scraping:
         for self.phonenumber in soup.find_all('div', {'class': 'blEntry phone'}):
             print("Telefonnummer: " + self.phonenumber.text)
 
-        #
+
+        #define auto increment ID for each Restaurant, Review and User 
+        self.idRestaurant = 0
+        self.idReview = 0
+        self.idUser = 0
+
+
         #load all relevant restaurant data into database (table: RESTAURANTS)    
         self.parse_to_tinydb_rest()   
-        #       
-        #
-
+        
 
         #get all the review containers from current page
         review_containers = soup.find_all('div', class_= 'review-container')
         reviewsPerPage = len(review_containers)
 
-        
+
         #get the pageNumbers for all reviews per restaurant
         pageNumbers = soup.find_all('a', class_= 'pageNum')
         pages = len(pageNumbers)
         counter = 1
         
+
         #Extract data from single containers   
         for container in review_containers:
                 
@@ -97,14 +99,18 @@ class Web_scraping:
             #pass the new_url to the loop_trough_review_pages function
             self.loop_through_review_pages(new_url)
         
+        
         print ("Es wurden " + str(counter) + " textuelle von " + self.rating_amount.string + " Bewertungen abgegeben!")
      
         
 
-
-    #loop through the restaurant review pages
+    '''   
+    @author: JohannesKnippel
+    
+    loop through the restaurant review pages and store the new link in variable
+    then pass it to the function where single data is scraped
+    '''
     def loop_through_review_pages(self, loop_url):
-        
         source_code = requests.get(loop_url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, "html.parser")
@@ -123,24 +129,26 @@ class Web_scraping:
 
     
 
-
-    #loop through the reviews and scrape the data
-    def get_single_review_data(self,review_url):
-            
+    '''   
+    @author: JohannesKnippel, anjawolf, JohannaSickendiek
+    
+    loop through the reviews and scrape the data. use link of loop_through_review_pages()
+    '''
+    def get_single_review_data(self,review_url):     
         source_code2 = requests.get(review_url)
         plain_text2 = source_code2.text
         soup = BeautifulSoup(plain_text2, "html.parser")
         
         #Lists to store scraped data in
         self.username = soup.find('span', {'class': 'expand_inline scrname'}).text
-        print('Username: ' + self.username)
+        print('Benutzername: ' + self.username)
         
         for self.numberOfReviews in soup.findAll('span', {'class': 'badgetext'})[0]:
-            print("Number of reviews: " + self.numberOfReviews.string)
+            print("Anzahl Reviews: " + self.numberOfReviews.string)
             
             
         for self.numberOfLikes in soup.findAll('span', {'class': 'badgetext'})[1]:
-            print("Number of likes: " + self.numberOfLikes.string)
+            print("Anzahl Likes: " + self.numberOfLikes.string)
 
         #intizialise and load the json from TA to search for relevant data     
         self.item_name = soup.find('script', {'type': 'application/ld+json'})
@@ -162,31 +170,33 @@ class Web_scraping:
         #Get URL from review pictures
         for link in soup.findAll('img', {'class': 'centeredImg'}):
             self.src = link.get('src')
-            print("source of picture: " + self.src)
- 
-    
+            print("Quelle Bild " + self.src)
+        
+        #load all relevant review, User, Picture data into database (table: REVIEWS, USERS, PICTURES)
+        self.parse_to_tinydb_us_rev_pic()
     
 
 
-    '''
-    Created on 18.04.2018
-    
+    '''    
     @author: JohannesKnippel
     
-    TinyDB - Database. Scraped Data from get_single_data() function will be parsed to a Database saved in .json-Format.
+    TinyDB - Database. Function to handle exit of programm when pressing button on popup.Window
     '''
-
     def endProgram(self):
         exit()
 
 
-    #TinyDB
+
+    '''    
+    @author: JohannesKnippel
+    
+    TinyDB - Database. Scraped Data from get_single_data() function will be parsed to a Database saved in .json-Format. These are all the Restaurant related data
+    '''
     def parse_to_tinydb_rest(self):
-        
         cwd = os.getcwd()
         try:
             #create the database or use the existing one
-            db = TinyDB('db.json')
+            db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': '))
             #create the tables inside the database
             tableReviews = db.table('REVIEWS')
             tableUsers = db.table('USERS')
@@ -195,12 +205,20 @@ class Web_scraping:
         
         except:
             print("Error opening file")    
-        
-        #################################bis hier 19.04.18###############
-        # Idee: 4 tabellen mit drei IDs: Hotel_ID, User_ID, Review_ID
-        # Implementieren dieser IDs per auto increment funkkion.
-        
-        #define the data to insert into database including an auto increment ID for each Table  
+
+
+        #check if there are already Restaurants in the table, if so, count the Restaurants and set R_ID. check also if the newly set R_ID already exists, if so, count and check again
+        if tableRestaurants.contains((where('R_ID') > 0)):
+            for ids3 in tableRestaurants:
+                self.idRestaurant = self.idRestaurant + 1
+                if tableRestaurants.contains((where('R_ID') == self.idRestaurant)):
+                    # print ("next")
+                    x = 1
+                else:
+                    print ("jetzt wird neues Restaurant mit RestaurantID : " + str(self.idRestaurant) + " eingefügt")
+                    break
+        else:
+            self.idRestaurant = 1
 
         #HANDLE RESTAURANTS
         #check if there already exists an entry in the Restaurans-table with the same name and the same address (these two attributes don't change/are not variable so there is a more constant way to check for duplicates)
@@ -220,84 +238,94 @@ class Web_scraping:
             self.popularity2 = unicodedata.normalize("NFKD", self.popularity.text)
             self.cuisine2 = unicodedata.normalize("NFD", self.cuisine.text)
             self.address2 = unicodedata.normalize("NFKD", self.address.string)
-            self.price_level2 = unicodedata.normalize("NFKC", self.price_level.string)
-            self.idRestaurant = 1  
-            dataRestaurants = {'ID':self.idRestaurant, 'RESTAURANTNAME':self.name2, 'PUNKTESKALA':self.overallPoints.div.span['content'], 'ANZAHL_BEWERTUNGEN':self.rating_amount.string, 'POPULARITAET':self.popularity2, 'PREIS_LEVEL':self.price_level2, 'KUECHE':self.cuisine2, 'STRASSE':self.address2, 'PLZ_ORT':self.locality.string, 'TELEFONNUMMER':self.phonenumber.text}
+            self.price_level2 = unicodedata.normalize("NFKC", self.price_level.string) 
+            dataRestaurants = {'R_ID':self.idRestaurant, 'RESTAURANTNAME':self.name2, 'PUNKTESKALA':self.overallPoints.div.span['content'], 'ANZAHL_BEWERTUNGEN':self.rating_amount.string, 'POPULARITAET':self.popularity2, 'PREIS_LEVEL':self.price_level2, 'KUECHE':self.cuisine2, 'STRASSE':self.address2, 'PLZ_ORT':self.locality.string, 'TELEFONNUMMER':self.phonenumber.text}
             tableRestaurants.insert(dataRestaurants) 
-        
-        
-                  
-        print(tableRestaurants.all())
-        ft = Query()
-        suche = tableRestaurants.search(ft.name == self.name.string)
-        print(suche) 
 
 
+
+    '''    
+    @author: JohannesKnippel
+    
+    TinyDB - Database. Scraped Data from get_single_review_data() function will be parsed to a Database saved in .json-Format. These are all the Review, Users and Pictures related data
+    '''
     def parse_to_tinydb_us_rev_pic(self):
-        print("Hello")
+        cwd = os.getcwd()
+        try:
+            #create the database or use the existing one
+            db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': '))
+            #create the tables inside the database
+            tableReviews = db.table('REVIEWS')
+            tableUsers = db.table('USERS')
+            tablePictures = db.table('PICTURES')
+            tableRestaurants = db.table('RESTAURANTS')
         
-        #HANDLE REVIEWS
-        #check if there already exists an entry in the REVIEWSs-table with the same name and the same address (these two attributes don't change/are not variable so there is a more constant way to check for duplicates)
-#        if tableReviews.contains((where('++') == self.name.string) & (where('address') == self.address.string)): 
-#            #pop up a message box
-#            msg = "Dieses Hotel hast du bereits gesucht und ist in der Dtaenbak hinterlegt!"
-#            popup = tk.Tk()
-#            popup.wm_title("!")
-#            label = ttk.Label(popup, text=msg)
-#            label.pack(side="top", fill="x", pady=10)
-#            B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-#            B1.pack()
-#            popup.mainloop()
-         #if there is no such entry, parse the data into the corresponding table of the database and define the data to insert into database including an auto increment ID for each Table       
-#        else:
-#            idReview =   
-#            dataReviews = {'fruit':'orange', 'price':25}
-#            tableReviews.insert(dataReviews)
-        
-        
+        except:
+            print("Error opening file") 
+
+
+        #AUTOINCREMENT U_ID: check if there are already Users in the table, if so, count the Users and set U_ID. check also if the newly set U_ID already exists, if so, count and check again
+        if tableUsers.contains((where('U_ID') > 0)):
+            for ids in tableUsers:
+                self.idUser = self.idUser + 1
+                if tableUsers.contains((where('U_ID') == self.idUser)):
+                    #print ("next")
+                    x = 1
+                else:
+                    print ("jetzt wird neuer User mit UserID : " + str(self.idUser) + " eingefügt")
+                    break
+        else:
+            self.idUser = 1
+
+
+        #AUTINCREMENT REV_ID: check if there are already Reviwes in the table, if so, count the Reviews and set REV_ID. check also if the newly set REV_ID already exists, if so, count and check again
+        if tableReviews.contains((where('REV_ID') > 0)):
+            for ids2 in tableReviews:
+                self.idReview = self.idReview + 1
+                if tableReviews.contains((where('REV_ID') == self.idReview)):
+                    #print ("next")
+                    x = 1
+                else:
+                    print ("jetzt wird neuer Review mit ReviewID : " + str(self.idReview) + " eingefügt")
+                    break
+        else:
+            self.idReview = 1
+
         
         #HANDLE USERS
-        #check if there already exists an entry in the USERS-table with the same name and the same address (these two attributes don't change/are not variable so there is a more constant way to check for duplicates)
-#        if tableUsers.contains((where('name') == self.name.string) & (where('address') == self.address.string)): 
-#            #pop up a message box
-#            msg = "Dieses Hotel hast du bereits gesucht und ist in der Dtaenbak hinterlegt!"
-#            popup = tk.Tk()
-#            popup.wm_title("!")
-#            label = ttk.Label(popup, text=msg)
-#            label.pack(side="top", fill="x", pady=10)
-#            B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-#            B1.pack()
-#            popup.mainloop()
+        #check if there already exists an entry in the USERS-table with the same name (this attribute doesn't change/is not variable so there is a more constant way to check for duplicates)
+        if tableUsers.contains((where('BENUTZERNAME') == self.username) & (where('REV_ID') == self.idReview)): 
+            #print a message
+            msg = ("Der User" + self.username + " ist bereits in der Datenbank hinterlegt! Es wird weitergesucht...")
+            print(msg)
         #if there is no such entry, parse the data into the corresponding table of the database and define the data to insert into database including an auto increment ID for each Table       
-#        else:
-#            idUser = 
-#            dataUsers = {'fruit':'orange', 'price':25}  
-#            tableUsers.insert(dataUsers)
-        
-        
-        
+        else:
+            dataUsers = {'U_ID':self.idUser, 'REV_ID':self.idReview, 'BENUTZERNAME':self.username, 'ANZAHL_REVIEWS':self.numberOfReviews, 'ANZAHL_LIKES':self.numberOfLikes}  
+            tableUsers.insert(dataUsers)
+
+
+        #HANDLE REVIEWS
+        #check if there already exists an entry in the REVIEWSs-table with the same name and the same address (these two attributes don't change/are not variable so there is a more constant way to check for duplicates)
+        if tableReviews.contains((where('TITEL') == self.title) & (where('BEWERTUNG') == self.review)): 
+            #print a message
+            msg = ("Dieses Review mit dem Titel: " + self.title + " wurde bereits in der Datenbank hinterlegt! Es wird weitergesucht...")
+            print(msg)
+        #if there is no such entry, parse the data into the corresponding table of the database and define the data to insert into database including an auto increment ID for each Table       
+        else:
+            dataReviews = {'R_ID':self.idRestaurant, 'U_ID':self.idUser, 'REV_ID':self.idReview, 'TITEL':self.title, 'BEWERTUNG':self.review, 'RATING':self.points.span.span['alt'].split()[0]}
+            tableReviews.insert(dataReviews)
+            
+               
         #HANDLE PICTURES
         #check if there already exists an entry in the PICTURES-table with the same name and the same address (these two attributes don't change/are not variable so there is a more constant way to check for duplicates)
-#        if tablePictures.contains((where('name') == self.name.string) & (where('address') == self.address.string)): 
-#            #pop up a message box
-#            msg = "Dieses Hotel hast du bereits gesucht und ist in der Dtaenbak hinterlegt!"
-#            popup = tk.Tk()
-#            popup.wm_title("!")
-#            label = ttk.Label(popup, text=msg)
-#            label.pack(side="top", fill="x", pady=10)
-#            B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-#            B1.pack()
-#            popup.mainloop()
+        if tablePictures.contains((where('QUELLE') == self.src)): 
+            #pop up a message box
+            msg = ("Dieses Bild mit dem Link: " + self.src + " wurde bereits in der Datenbank hinterlegt! Es wird weitergesucht...")
+            print(msg)
         # if there is no such entry, parse the data into the corresponding table of the database and define the data to insert into database including an auto increment ID for each Table       
-#        else:
-#            idPicture =   
-#            dataPictures = {'fruit':'orange', 'price':25} 
-#            tableRestaurants.insert(dataRestaurants)
-
-
-
-
-
+        else:  
+            dataPictures = {'REV_ID':self.idReview, 'QUELLE':self.src} 
+            tablePictures.insert(dataPictures)
 
 
 
@@ -307,7 +335,6 @@ class Web_scraping:
    
     opens a GUI that validates a given url-string and starts the scraping on button click
     '''
-
     def start_GUI(self):
 
         roots = Tk()
@@ -333,7 +360,6 @@ class Web_scraping:
             else:
                 messagebox.showinfo("Vaildation successful", "Url seems to be valid")
 
-
         check_url_Button= Button(roots, text='Validate Url', command=check_url)
 
         def go_scrape():
@@ -346,12 +372,17 @@ class Web_scraping:
     
         roots.mainloop()
 
-           
 
+
+'''
+@author: Skanny Morandi, JohannesKnippel
+   
+Maisn function, will be executed when starting the python-script
+'''         
 if __name__ == "__main__":
     ws = Web_scraping()
     ws.start_GUI()
-
+    #example URL:
     #url = "https://www.tripadvisor.de/Restaurant_Review-g946452-d8757235-Reviews-The_Forge_Tea_Room-Hutton_le_Hole_North_York_Moors_National_Park_North_Yorkshire_.html"
 
 
